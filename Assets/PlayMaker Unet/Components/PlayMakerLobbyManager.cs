d975b237618766862e2375c8f1f913266504a77b
jeanfabre <contact@fabrejean.net>
2016-02-12 10:01:22 +0300

    - Started own duplicate of LobbyScene - Pseudo prefab for PlayMakerLobbyManager - Empty Main Scene to start PlayMaker integration

---
+++ Assets/PlayMaker Unet/Components/PlayMakerLobbyManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayMakerLobbyManager : NetworkLobbyManager {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
